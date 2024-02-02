c24757afffb529ba05454f5685c80223f756c3e7
NCnight <aliefhafshah10@gmail.com>
2024-02-02 08:26:17 +0700

    nambah jam

--- resources/views/dashGuru.blade.php
+++ resources/views/dashGuru.blade.php
@@ -22,23 +22,96 @@
             flex-direction: column;
             align-items: center;
             justify-content: center;
-            background-color: #fff;
+            background-color: #ffe23b;
             padding: 20px;
             border-radius: 10px;
             width: 550px;
             height: 270px;
             margin: 0 auto;
+            margin-right: 15px;
+            margin-left: 15px;
+        }
+
+        .bubble-left {
+            width: 30%;
+        }
+
+        .bubble-center {
+            width: 40%;
+        }
+
+        .bubble-right {
+            width: 30%;
         }
 
         .bubble input[type=text] {
             margin: 20px auto 0;
             align-self: center;
         }
+
+        .analog-clock {
+            width: 200px;
+            height: 200px;
+            border-radius: 50%;
+            background-color: #fff;
+            position: relative;
+            box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.7);
+        }
+
+        .analog-clock::before {
+            content: '';
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 15px;
+            height: 15px;
+            background-color: #333;
+            border-radius: 50%;
+            z-index: 1;
+        }
+
+        .analog-clock .hour-hand,
+        .analog-clock .minute-hand {
+            position: absolute;
+            background-color: #333;
+            border-radius: 5px;
+            transform-origin: bottom center;
+            z-index: 2;
+        }
+
+        .analog-clock .hour-hand {
+            width: 6px;
+            height: 40%;
+            top: 30%;
+            left: 50%;
+            transform: translateX(-50%);
+        }
+
+        .analog-clock .minute-hand {
+            width: 4px;
+            height: 45%;
+            top: 10%;
+            left: 50%;
+            transform: translateX(-50%);
+        }
+
+        .analog-clock .second-hand {
+            display: none;
+        }
     </style>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 overflow-auto">
-            <div class="bubble">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 overflow-auto flex justify-between">
+            <div class="bubble bubble-left">
+                <!-- Bubble kiri -->
+                <div class="analog-clock">
+                    <div class="hour-hand"></div>
+                    <div class="minute-hand"></div>
+                </div>
+            </div>
+
+            <div class="bubble bubble-center">
                 <form name="nipd_inpt" action="{{ route('dashGuru.search') }}" method="POST" class="mb-5">
                     @csrf
                     <input id="textarea" type="text"
@@ -71,6 +144,9 @@
                         </ul>
                     @endif
                 </body>
+            </div>
+            <div class="bubble bubble-right">
+                <!-- Bubble kanan -->
             </div>
         </div>
     </div>
